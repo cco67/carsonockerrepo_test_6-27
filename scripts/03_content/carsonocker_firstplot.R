4554aefdaaf0bb0396759e22cc903ea8580f3d15
edresser <emerson.dresser@gmail.com>
2026-09-03 09:35:12 -0400

    Script update as well

--- scripts/03_content/carsonocker_firstplot.R
+++ scripts/03_content/carsonocker_firstplot.R
@@ -7,10 +7,11 @@ data(data_lionfish)
 # Create a simple plot
 p <- ggplot(data_lionfish,
             aes(x = total_length_mm, y = total_weight_gr)) +
-  geom_point()
+  geom_point(color="steelblue")+
+  labs(x="Total length mm", y="Total Weight (gr)")
 
 p
 
 # Save plot
 # The above shouldn't work, why?
-ggsave(plot = p, filename = "results/img/first_plot.png")
+ggsave(plot = p, filename = "results/img/second_plot.png")
